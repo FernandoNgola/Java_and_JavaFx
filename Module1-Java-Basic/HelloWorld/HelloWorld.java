a2c3edf2c36d8615236813faea875358dcd07d5c
FernandoNgola <48277773+FernandoNgola@users.noreply.github.com>
2020-08-29 21:58:32 +0100

    Update HelloWorld.java

--- Module1-Java-Basic/HelloWorld/HelloWorld.java
+++ Module1-Java-Basic/HelloWorld/HelloWorld.java
@@ -1,7 +1,7 @@
 package HelloWorld;
 
 public class HelloWorld{
-  public static void main(Strings args[]){
+  public static void main(String[] args){
       System.out.println("Hello World!");
   }
 }
